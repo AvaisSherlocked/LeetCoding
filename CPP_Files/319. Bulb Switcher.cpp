3143c2a2f8d0e589b8dba1ed47efffcaa2a4038f
AvaisSherlocked <38180982+AvaisSherlocked@users.noreply.github.com>
2018-06-03 10:53:20 +0800

    math: sqrt(), if num is smaller, use bit operation

---
+++ CPP_Files/319. Bulb Switcher.cpp
@@ -0,0 +1,50 @@
+/* questions:
+There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
+
+Example:
+
+Input: 3
+Output: 1 
+Explanation: 
+At first, the three bulbs are [off, off, off].
+After first round, the three bulbs are [on, on, on].
+After second round, the three bulbs are [on, off, on].
+After third round, the three bulbs are [on, off, off]. 
+
+So you should return 1, because there is only one bulb is on.
+*/
+
+// not work -- bit operation is not fit in big number failed at case: n = 99
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        long m = pow(2,n)-1;
+        for(int i=2;i<=n;i++){
+            m=m^helper(n,i);
+        }
+        int c=0; 
+        // count how much '1' in m
+        while(m>0){
+            long t=m>>1<<1;
+            c += m-t;
+            m = m>>1;
+        }
+        return c;
+    }
+    long helper(int n,int i){
+        long res=0;
+        int d = i;
+        while(n-i>=0){
+            res += pow(2,n-i);
+            i += d;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
